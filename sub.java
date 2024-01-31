1e4e289fab95fcfd71ac9eb749f35966e635597c
Nilufa Yesmin Ritu <nilufa691416@gmail.com>
2024-02-01 01:50:50 +0600

    Sub

---
+++ sub.java
@@ -0,0 +1,9 @@
+public class sub {
+        public static void main (String []args){
+            int a=30, b=20, sub;
+            sub=a-b;
+            System.out.println("The subtraction is : " + sub );
+        }
+    
+    
+}
